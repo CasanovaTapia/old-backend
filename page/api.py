9f24cccce44ee2b945e01469406cf45f7082c3a0
Dave Chason <chason@lapel.co>
2015-02-14 07:33:20 +0000

    Fixed Order Resource one2one instrospection error with Client Resource

--- page/api.py
+++ page/api.py
@@ -3,21 +3,24 @@ from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from orders.models import Order
 from clients.models import Client
+from tastypie import fields
 
 class UserResource(ModelResource):
-    class Meta:
-        queryset = User.objects.all()
-        resource_name = 'user'
-	authorization= Authorization()
-
-class OrderResource(ModelResource):
-    class Meta:
-        queryset = Order.objects.all()
-        resource_name = 'orders'
-	authorization= Authorization()
-
+	class Meta:
+		queryset = User.objects.all()
+		resource_name = 'user'
+		authorization= Authorization()
+	
 class ClientResource(ModelResource):
 	class Meta:
 		queryset = Client.objects.all()
-		resource_name = "clients"
+		resource_name = "client"
 		authorization= Authorization()
+
+class OrderResource(ModelResource):
+	client = fields.ForeignKey(ClientResource, 'client')
+	class Meta:
+		queryset = Order.objects.all()
+		resource_name = 'orders'
+		authorization= Authorization()
+	
